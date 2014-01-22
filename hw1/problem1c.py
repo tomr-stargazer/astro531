db5f8132661ec02ce200e31e5d33771d432efef0
Tom Rice <t.rice90@gmail.com>
2014-01-21 22:51:52 -0500

    Initial commit.

---
+++ hw1/problem1c.py
@@ -0,0 +1,17 @@
+"""
+Code to make calculations & plots for Problem 1c.
+
+"Plot the spectral energy distribution 
+log (nu F_nu) vs. log (lambda).
+Also for comparison overplot 
+log (nu F_nu) vs. log (lambda)
+calculated with fluxes not corrected for reddening."
+
+"""
+
+from __future__ import division
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
