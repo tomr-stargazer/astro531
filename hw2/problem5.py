7fc5b9e8805459aeb2c2f91c8cc49e8b69f76e53
Tom Rice <t.rice90@gmail.com>
2014-02-01 18:32:33 -0500

    Wrote the solution to the first part of problem 5!
    I found X, Y, and Z.

--- hw2/problem5.py
+++ hw2/problem5.py
@@ -5,3 +5,39 @@ Astro 531 homework #2.
 """
 
 from __future__ import division
+
+import numpy as np
+
+import astropy
+
+cowley_table = astropy.table.Table.read("Table3.2.Cowley.txt", format='ascii')
+
+cowley_table.rename_column('col1', 'element')
+cowley_table.rename_column('col2', 'symbol')
+cowley_table.rename_column('col3', 'atomic_number')
+cowley_table.rename_column('col4', 'atomic_weight')
+cowley_table.rename_column('col5', 'log_abundance')
+
+def de_logiffy(dex_abundance):
+    return 10**(dex_abundance - 12)
+
+total_mass = np.sum(
+    de_logiffy(cowley_table['log_abundance'])*cowley_table['atomic_weight'])
+
+print total_mass
+
+# X: mass fraction of H.   
+# Y: mass fraction of He.
+# Z: mass fraction of "metals"
+
+X = (de_logiffy(cowley_table['log_abundance'][0]) * 
+     cowley_table['atomic_weight'][0]) / total_mass
+
+Y = (de_logiffy(cowley_table['log_abundance'][1]) *
+     cowley_table['atomic_weight'][1]) / total_mass
+
+Z = 1 - X - Y
+
+print "X: " + str(X)
+print "Y: " + str(Y)
+print "Z: " + str(Z)
