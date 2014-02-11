640ebfa14ea23b39eacac7bfd3b1ae50954d011b
Tom Rice <t.rice90@gmail.com>
2014-02-11 18:22:29 -0500

    Getting the fermidirac table to play nice.

--- hw3/problem2.py
+++ hw3/problem2.py
@@ -8,6 +8,14 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
+import astropy.table
+
+fermidirac_table = astropy.table.Table.read("FermiDiracIntegrals.txt", format='ascii', data_start=1)
+
+fermidirac_table.rename_column('col1', 'alpha')
+fermidirac_table.rename_column('col2', '2/3 F_3/2')
+fermidirac_table.rename_column('col3', 'F_1/2')
+
 
 def occupation_index(alpha, energy):
     """ Calculates the occupation index P(p). """
