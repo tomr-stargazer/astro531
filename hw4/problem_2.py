d30b228748cb6858a49d822b6598272cc694c266
Tom Rice <t.rice90@gmail.com>
2014-02-22 23:47:04 -0500

    Wrote a long docstring for problem2.

---
+++ hw4/problem_2.py
@@ -0,0 +1,23 @@
+"""
+The standard model is an approximation in which radiative, 
+homogeneous stars are modeled by a polytrope of index n=3.
+Using the standard model approximation, calculate the structure of the sun,
+i.e. P(r) and rho(r). 
+Chuck Cowley kindly provided the Lane-Emden function of index n=3 and 
+you can find it in the file "Polytrope_n3.txt". 
+Hint: Calculate the value of beta with at least 7 decimals; I found:
+beta = 0.9995853.
+
+The file bs05op.dat contains the "Standard Solar Model". 
+This model is the preferred model for the interior of the sun 
+(it does not include the atmosphere), 
+calculated by Bahcall et al. 2005.
+It reproduces the depth of the solar convection zone as inferred from 
+heliseismological data and the measured neutrino flux.
+The columns are self-explanatory.
+
+Overplot the standard model values of P(r) and rho(r) on the values
+of P(r) and rho(r) from the "Standard Solar Model".
+Comment on the goodness of the polytropic approximation for the sun.
+
+"""
