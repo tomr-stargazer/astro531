c3ec042a9d1ba595b14a19558807fbdcb2b1ed03
Tom Rice <t.rice90@gmail.com>
2014-02-25 16:37:15 -0500

    Made some non-functioning changes to problem5. It never worked.

--- hw4/problem_5.py
+++ hw4/problem_5.py
@@ -11,13 +11,25 @@ import astropy
 import astropy.units as u
 import astropy.constants as c
 
-def ionization_fraction_y():
+chi_H = 13.6 * u.eV
+
+def ionization_fraction_y(temperature, density):
     """
     Ionization fraction 'y' following Clayton's notation.
 
     """
 
-    # y = H+ / (H + H+)
+    T = u.quantity.Quantity(temperature, u.K)
+    rho = u.quantity.Quantity(density, u.g / u.cm**3)
+
+    saha_g = ((2 * np.pi * c.m_e * c.k_B * T)**(3/2) / c.h**3 * np.exp(- chi_H / (c.k_B * T) )).decompose().value
+
+    Hplus = 1/2 * ( ( ((c.u + c.m_e)*saha_g)**2 + 4*saha_g*rho)**(1/2) -
+                    (c.u + c.m_e)*saha_g )
+
+    H_plus_Hplus = rho / c.u
+
+    y = Hplus / H_plus_Hplus
 
     return y
 
@@ -27,14 +39,16 @@ def gamma3_minus_one(temperature, density ):
 
     """
 
-    T = temperature
-    chi_H = 13.6 * u.eV
+    T = u.quantity.Quantity(temperature, u.K)
+
+    y = ionization_fraction_y(temperature, density)
 
     D = y*(1-y)/( (2-y) * (1+y) )
 
     threehalfs_plus_chiH_over_kT = 3/2 + chi_H/(c.k_B * T) 
 
-    reduced_gamma = (2 + 2*D * threehalfs_plus_chiH_over_kT ) / (3 + 2*D * threehalfs_plus_chiH_over_kT**2 ) 
+    reduced_gamma = ((2 + 2*D * threehalfs_plus_chiH_over_kT ) /
+                     (3 + 2*D * t`xhreehalfs_plus_chiH_over_kT**2 ) ) 
 
     return reduced_gamma
 
