2ba6e69a911ba4abd5bb64da68b58e5001ae7f4c
Tom Rice <t.rice90@gmail.com>
2014-03-30 21:38:09 -0400

    Drafting hw6/problem4b.

---
+++ hw6/problem_4b.py
@@ -0,0 +1,12 @@
+"""
+problem_4b.py
+
+Astro 531 HW #6, problem 4
+
+Check how well the slopes predictd in part (a) fit the data. 
+Plot log L/Lsun vs. log M/Msun using data from the article
+"M/L relation of intermediate-mass stars" (Malkov 2007, MNRAS, 382, 1073).
+What criteria have been used to select the sample used in this article and 
+why is this appropriate? At what mass is the break between the two slopes?
+
+`"""
