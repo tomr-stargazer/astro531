9ae07d1b2236fd01f96eb4498e362af8f98f7b51
Tom Rice <t.rice90@gmail.com>
2014-04-15 23:01:30 -0400

    Wrote plotting code!

--- hw7/problem1.py
+++ hw7/problem1.py
@@ -6,6 +6,8 @@ Problem 1 from Nuria's homework.
 from __future__ import division
 
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
 import astropy.constants as c
 import astropy.units as u
 
@@ -42,5 +44,42 @@ def central_temperature(radius, mean_molecular_weight=mu, mass=M, n=3/2):
 
 	return T_c.decompose()
 
+t_array = np.logspace(11, 18, 100)
+t_array_years = t_array / (31557600.0)
+
+hr_array = hayashi_radius(t_array)
+Tc_array = central_temperature(hr_array)
+
+def plot_problem1d():
+	fig1 = plt.figure()
+	plt.plot(t_array_years, (hr_array/c.R_sun).value, lw=2)
+	plt.title("Hayashi radius")
+	plt.xlabel("Time (years)")
+	plt.ylabel(r"Hayashi radius ($R_\odot$)")
+	plt.xlim(t_array_years[0], t_array_years[-1])
+	plt.semilogx()
+
+	fig2 = plt.figure()
+	plt.plot(t_array_years, Tc_array.value, lw=2)
+	plt.title("Central temperature")
+	plt.ylabel("Central temperature (K)")
+	plt.xlabel("Time (years)")
+	plt.xlim(t_array_years[0], t_array_years[-1])
+	plt.semilogx()
+
+	plt.show()
+
+	return fig1, fig2
 
 
+plt.rcParams['axes.linewidth'] = 1.5
+plt.rcParams['xtick.minor.size'] = 1.5
+plt.rcParams['ytick.minor.size'] = 1.5
+plt.rcParams['xtick.major.size'] = 2.0
+plt.rcParams['ytick.major.size'] = 2.0
+font = {'family' : 'normal',
+	'weight' : 'normal',
+	'size' : 14}
+matplotlib.rc('font', **font)
+
+fig1, fig2 = plot_problem1d()
